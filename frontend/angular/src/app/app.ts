e7e3a3523a0f7a95f12cf0a672a4db5acf3c1f66
Tim Montague <tim.john.montague@gmail.com>
2026-01-11 08:33:20 -0500

    AngularScaffold: Add ThemeService and wire light/dark theming

--- frontend/angular/src/app/app.ts
+++ frontend/angular/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/theme/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
+
 export class App {
+  public readonly themeService = inject(ThemeService);
   protected readonly title = signal('angular');
+
+  constructor() {
+    this.themeService.initialize();
+  }
+
+
+
+
 }
